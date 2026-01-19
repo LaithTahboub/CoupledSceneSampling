f296217b05493764d0616b67820580610248f907
Laith Tahboub <ltahboub@vulcan24.umiacs.umd.edu>
2026-01-19 02:10:41 -0500

    more finetuning

--- scripts/finetune_dreambooth.sh
+++ scripts/finetune_dreambooth.sh
@@ -3,18 +3,18 @@
 #SBATCH --partition=vulcan-ampere
 #SBATCH --ntasks=1
 #SBATCH --mem=48gb
-#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --gres=gpu:rtxa6000:2
 #SBATCH --account=vulcan-jbhuang
 #SBATCH --qos=vulcan-medium
 #SBATCH --time=24:00:00
 #SBATCH --output=finetuning_dreambooth.out
 #SBATCH --error=finetuning_dreambooth.err
 
-export MODEL_NAME="Manojb/stable-diffusion-2-1-base"
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
 # export INSTANCE_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/train_ready_data"
 export INSTANCE_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/test_final/Mysore_Palace/images/commons/East_side_of_the_Mysore_Palace/0/pictures"
-export CLASS_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/class_data"  # <--- Added this directory for generic images
-export OUTPUT_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd21"
+export CLASS_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/class_data"
+export OUTPUT_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd3_med"
 
 # accelerate launch --mixed_precision="fp16" train_dreambooth.py \
 #   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -41,17 +41,48 @@ export OUTPUT_DIR="/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace
 #   --push_to_hub
 #
 source /fs/nexus-scratch/ltahboub/CoupledSceneSampling/.venv/bin/activate
-accelerate launch /fs/nexus-scratch/ltahboub/CoupledSceneSampling/scripts/train_dreambooth.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
-  --instance_data_dir=$INSTANCE_DIR \
-  --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of sks palace" \
-  --resolution=768 \
-  --train_batch_size=2 \
-  --gradient_accumulation_steps=1 \
-  --learning_rate=5e-6 \
-  --lr_scheduler="constant" \
-  --lr_warmup_steps=0 \
-  --max_train_steps=400 \
-  --push_to_hub \
-  --report_to="wandb"
+# accelerate launch /fs/nexus-scratch/ltahboub/CoupledSceneSampling/scripts/train_dreambooth.py \
+#   --pretrained_model_name_or_path=$MODEL_NAME \
+#   --instance_data_dir=$INSTANCE_DIR \
+#   --output_dir=$OUTPUT_DIR \
+#   --instance_prompt="a photo of sks palace" \
+#   --resolution=768 \
+#   --train_batch_size=2 \
+#   --gradient_accumulation_steps=1 \
+#   --learning_rate=5e-6 \
+#   --lr_scheduler="constant" \
+#   --lr_warmup_steps=0 \
+#   --max_train_steps=400 \
+#   --push_to_hub \
+#   --report_to="wandb"
+
+  # accelerate launch /fs/nexus-scratch/ltahboub/CoupledSceneSampling/scripts/train_dreambooth.py \
+  #   --pretrained_model_name_or_path=$MODEL_NAME  \
+  #   --instance_data_dir=$INSTANCE_DIR \
+  #   --output_dir=$OUTPUT_DIR \
+  #   --instance_prompt="a photo of sks landmark" \
+  #   --resolution=1024 \
+  #   --train_batch_size=2 \
+  #   --gradient_accumulation_steps=1 \
+  #   --learning_rate=2e-6 \
+  #   --lr_scheduler="constant" \
+  #   --lr_warmup_steps=0 \
+  #   --max_train_steps=6000 \
+  #   --report_to="wandb"
+  #
+  accelerate launch \
+    --num_processes=2 \
+    /fs/nexus-scratch/ltahboub/CoupledSceneSampling/scripts/train_dreambooth_lora_sdxl.py \
+    --pretrained_model_name_or_path=$MODEL_NAME \
+    --instance_data_dir=$INSTANCE_DIR \
+    --output_dir=$OUTPUT_DIR \
+    --instance_prompt="a photo of sks landmark" \
+    --resolution=1024 \
+    --train_batch_size=1 \
+    --gradient_accumulation_steps=1 \
+    --learning_rate=1e-4 \
+    --lr_scheduler="constant" \
+    --lr_warmup_steps=0 \
+    --max_train_steps=3000 \
+    --mixed_precision=fp16 \
+    --report_to=wandb
--- scripts/inference.py
+++ scripts/inference.py
@@ -2,13 +2,13 @@ import torch
 from diffusers import StableDiffusionPipeline
 
 model_id = (
-    "/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd21"
+    "/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd15"
 )
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to(
     "cuda"
 )
 
-prompt = "A photo of sks palace at night"
-image = pipe(prompt, num_inference_steps=100, guidance_scale=10).images[0]
+prompt = "a photo of a sks landmark"
+image = pipe(prompt, num_inference_steps=50, guidance_scale=13.0).images[0]
 
 image.save("palace.png")
