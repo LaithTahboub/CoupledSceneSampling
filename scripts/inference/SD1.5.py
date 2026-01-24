c964195e77c22aaa7b71342fba324df1e8e42f92
Laith Tahboub <ltahboub@vulcan29.umiacs.umd.edu>
2026-01-24 07:16:29 -0500

    testing SVC

---
+++ scripts/inference/SD1.5.py
@@ -0,0 +1,21 @@
+import torch
+from diffusers import StableDiffusionPipeline, UNet2DConditionModel
+
+base_model_path = (
+    "/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd21"
+)
+checkpoint_path = "/fs/nexus-scratch/ltahboub/CoupledSceneSampling/mysore_palace_dreambooth_sd21/unet"
+
+unet = UNet2DConditionModel.from_pretrained(checkpoint_path, torch_dtype=torch.float16)
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    base_model_path, unet=unet, torch_dtype=torch.float16
+)
+
+pipe.to("cuda")
+
+prompt = (
+    "A hyperrealistic photo of sks palace, sunny day, clear blue sky, wide angle, landscape."
+)
+image = pipe(prompt, num_inference_steps=50, guidance_scale=11.0).images[0]
+image.save("palace.png")
--- scripts/retrieve_scenes.sh
+++
@@ -1,9 +0,0 @@
-bash /fs/nexus-scratch/ltahboub/CoupledSceneSampling/scripts/download_scene_data.sh "177/715" "Mysore_Palace" 0
-# bash download_data.sh "178/783" "Panch_Mahal" 0
-# bash download_data.sh "028/066" "Basilica_of_Bom_Jesus" 0
-# bash download_data.sh "275/801" "Qutb_Minar" 0
-# bash download_data.sh "159/326" "Victoria_Memorial" 0
-# bash download_data.sh "183/634" "Lotus_Temple_Hampi" 0
-# bash download_data.sh "000/216" "India_Gate" 0
-# bash download_data.sh "177/716" "Amber_Fort" 0
-# bash download_data.sh "259/261" "Jaisalmer_Fort" 0
